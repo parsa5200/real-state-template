323a1fed3276babf45df8c7f8d07da475981ddf4
parsa5200 <parsa.por5200@gmail.com>
2022-08-24 03:38:08 +0430

    responsive header

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import './App.scss';
-import { TopHeader, Header, Footer, ShowProperty } from './components';
-import { Home, AllProductsList } from './screens'
+import "./App.scss";
+import { TopHeader, Header, Footer, ShowProperty } from "./components";
+import { Home, AllProductsList } from "./screens";
 const App = () => {
   return (
     <BrowserRouter>
       <TopHeader />
       <Header />
-      <Routes>
+      {/* <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/productsList" element={<AllProductsList />} />
         <Route path="/flat/:slug" element={<ShowProperty />} />
       </Routes>
-      <Footer />
+      <Footer /> */}
     </BrowserRouter>
   );
-
 };
 
 export default App;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,51 +1,73 @@
-import React from "react"
-import { Link } from "react-router-dom"
-import './Header.scss'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Header.scss";
 
 const Header = () => {
-
-    return (
-        <div className="header">
-            <div className="container">
-                <nav className="navbar navbar-expand-lg navbar-light">
-                    <div className="container-fluid">
-                        <div >
-                            <a className='socialIcon' ><i className="fa-brands fa-instagram"></i></a>
-                            <a className='socialIcon' ><i className="fa-brands fa-whatsapp"></i></a>
-                            <a className='phoneIcon' ><i className="fa-light fa-phone-rotary me-1"></i>09381655200</a>
-                        </div>
-                        <div className="collapse navbar-collapse" id="navbarNav">
-                            <ul className="navbar-nav ms-auto">
-                                <li className="nav-item">
-                                    <Link className="nav-link" to="/contact">تماس با ما</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link " to="/blog">وبلاگ</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link" to="/about">درباره‌ ما</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link" to="#">نوع ملک <i className="fas fa-chevron-down"></i></Link>
-                                    <ul className="sub-ul">
-                                        <li><Link to="#">آپارتمان</Link></li>
-                                        <li><Link to="#">ویلا</Link></li>
-                                        <li><Link to="#">زمین</Link></li>
-                                    </ul>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link" to="/">خانه</Link>
-                                </li>
-                            </ul>
-                        </div>
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-                    </div>
-                </nav>
+  const [nav, setNav] = useState(false);
+  return (
+    <div className="header">
+      <div className="container">
+        <nav className="navbar navbar-expand-lg navbar-light">
+          <div className="container-fluid">
+            <div>
+              <a className="socialIcon">
+                <i className="fa-brands fa-instagram"></i>
+              </a>
+              <a className="socialIcon">
+                <i className="fa-brands fa-whatsapp"></i>
+              </a>
+              <a className="phoneIcon">
+                <i className="fa-light fa-phone-rotary me-1"></i>09381655200
+              </a>
             </div>
-        </div>
-    )
-}
+            <div>
+              <ul className={nav ? ["navbar-nav", "active"].join(" ") : "navbar-nav"}>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/contact">
+                    تماس با ما
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link " to="/blog">
+                    وبلاگ
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/about">
+                    درباره‌ ما
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="#">
+                    نوع ملک <i className="fas fa-chevron-down"></i>
+                  </Link>
+                  <ul className="sub-ul">
+                    <li>
+                      <Link to="#">آپارتمان</Link>
+                    </li>
+                    <li>
+                      <Link to="#">ویلا</Link>
+                    </li>
+                    <li>
+                      <Link to="#">زمین</Link>
+                    </li>
+                  </ul>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/">
+                    خانه
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div onClick={() => setNav(!nav)} className="mobil-toggle">
+              {nav ? <i className="fa-solid fa-xmark"></i> : <i className="fa-solid fa-bars"></i>}
+            </div>
+          </div>
+        </nav>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
