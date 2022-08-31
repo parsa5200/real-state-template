d49c146a9436d8788f155b7ca61d31ac75e0fb1f
parsa5200 <parsa.por5200@gmail.com>
2022-08-31 19:52:19 +0430

    axios fix

--- src/api/get/index.js
+++ src/api/get/index.js
@@ -1,23 +1,5 @@
-import Cookies from "js-cookie";
-import { logOuthandler } from "../../../helpers/function";
-import { constants } from "../../values";
-import routes from "../routes";
-import webSiteAxios from "../webSiteAxios";
-
-const dashboardData = async () => {
-  const url =
-    Cookies.get(constants.USER_TYPE) === "ADMIN" ? routes.getDashboardData : routes.superAgentGetDashboardData;
-  console.log({ url });
-  try {
-    const response = await webSiteAxios.get(url);
-    console.log({ response, url });
-    return response.data.result;
-  } catch (e) {
-    if (e.response.data.CODE === 5098) {
-      window.location.href = "/administrator/login";
-      logOuthandler();
-    }
-  }
+import products from "./product";
+const get = {
+  products,
 };
-
-export default dashboardData;
+export default get;
--- src/components/ProductsList/index.js
+++ src/components/ProductsList/index.js
@@ -1,35 +1,42 @@
-
-import axios from 'axios'
-import { TitleComponents, ProductItem, LoadingSvg } from "../../components"
-import { useState, useEffect } from "react"
-import "./ProductsList.scss"
-import { Link } from "react-router-dom"
+import axios from "axios";
+import { TitleComponents, ProductItem, LoadingSvg } from "../../components";
+import { useState, useEffect } from "react";
+import api from "../../api";
+import "./ProductsList.scss";
+import { Link } from "react-router-dom";
 const ProductsList = () => {
   const title = {
     text: "گالری املاک شاویلا",
     description: "جدیدترین آگهی‌های شما",
-  }
-  const [data, setData] = useState([])
-  const [isLoading, setIsLoading] = useState(true)
+  };
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const getProducts = async () => {
+    const products = await api.get.products();
+    console.log({ products });
+    setData(products.data);
+    setIsLoading(false);
+  };
+
   useEffect(() => {
-    axios.get("http://eflare.ir/api/v1/villas").then((response) => {
-      setData(response.data);
-      setIsLoading(false)
-      console.log(response.data);
-    });
-  }, [])
+    getProducts();
+  }, []);
 
   if (isLoading) {
-    return <div><LoadingSvg /></div>
+    return (
+      <div>
+        <LoadingSvg />
+      </div>
+    );
   }
   return (
-    <section className='Products-list'>
-      <div className='container'>
+    <section className="Products-list">
+      <div className="container">
         <TitleComponents title={title.text} description={title.description} />
-        <div className='products-list-container'>
-          {
-            data.properties.map((item) => {
-              return <ProductItem
+        <div className="products-list-container">
+          {data.properties.map((item) => {
+            return (
+              <ProductItem
                 slug={item.id}
                 description={item.title}
                 image={"http://eflare.ir" + item.images[0].path_thumbnail}
@@ -45,20 +52,20 @@ const ProductsList = () => {
                 constructionYear={item.year_of_construction}
                 propertyType={item.owner_name}
               />
-            })
-          }
+            );
+          })}
         </div>
         <div className="d-flex justify-content-center">
-          <Link to='/productsList' className='btn-more'>
+          <Link to="/productsList" className="btn-more">
             موارد بیشتر
           </Link>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default ProductsList
+export default ProductsList;
 
 // <ProductItem
 //               slug={item.sulg}
@@ -73,19 +80,19 @@ export default ProductsList
 //               key={index}
 //             />
 
-  // const [products, setProducts] = useState([])
-  // useEffect(() => {
-  //   const fetchAPI = async () => {
-  //     const result = await axiosInstance({ url: "http://eflare.ir/api/v1/villas", method: "get" })
-  //     console.log({ result })
-  //     return result
-  //   }
-  //   fetchAPI()
-  // }, [])
+// const [products, setProducts] = useState([])
+// useEffect(() => {
+//   const fetchAPI = async () => {
+//     const result = await axiosInstance({ url: "http://eflare.ir/api/v1/villas", method: "get" })
+//     console.log({ result })
+//     return result
+//   }
+//   fetchAPI()
+// }, [])
 
-  // async function fetchAPI() {
-  //   const result = await axiosInstance({ url: "http://eflare.ir/api/v1/villas", method: "get" })
-  //   console.log({ result })
-  //   return result
-  // }
-  // fetchAPI()
+// async function fetchAPI() {
+//   const result = await axiosInstance({ url: "http://eflare.ir/api/v1/villas", method: "get" })
+//   console.log({ result })
+//   return result
+// }
+// fetchAPI()
